8bfc97eaabad62ed76fa707e0f114ab7fe2e5000
Bastien CLEMENT <hello@lechatgraphique.fr>
2021-02-25 09:47:41 +0100

    Component navbar

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  brand: string = '!Freezy';
+
   constructor() { }
 
   ngOnInit(): void {
